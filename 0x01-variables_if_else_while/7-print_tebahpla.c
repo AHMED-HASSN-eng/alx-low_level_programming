c612390b97aa6e11153784b91b0d5f37a34404d3
ahmed <hasnm2287@example.com>
2023-03-17 17:53:08 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 122;
+while (n > 96)
+{
+putchar(n);
+n--;
+}
+putchar(10);
+return (0);
+}
