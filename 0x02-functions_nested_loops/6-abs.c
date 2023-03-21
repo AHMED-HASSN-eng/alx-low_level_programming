182a79ded117cf3602f9f38e479056ff409956d3
ahmed <hasnm2287@example.com>
2023-03-21 09:09:44 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - print abseluot of number
+ *@n: the number
+ * Return: abseluot number
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return ((-1 * n));
+}
+}
