25aef4bd6fea8434074105873f2ffcee4ae199cb
ahmed <hasnm2287@example.com>
2023-03-21 08:23:41 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - print is pos or neg or zero
+ *@n: the number
+ * Return: 1 is pos 0 is zero -1 is neg
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43)
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48)
+return (0);
+}
+else
+{
+_putchar(45)
+return (-1);
+}
+}
