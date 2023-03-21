d0b06e99273ac705f15f9ccc39fc1a67050ae325
ahmed <hasnm2287@example.com>
2023-03-21 16:22:28 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add -  add two number
+ * @n: first number
+ * @m: second number
+ * Return: prints every minute of the day
+ */
+int add(int n, int m)
+{
+return (n + m);
+}
