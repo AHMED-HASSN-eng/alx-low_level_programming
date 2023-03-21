4842adf8fe6846b7277fcad313bc44606cfabb3d
ahmed <hasnm2287@example.com>
2023-03-21 08:06:29 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - print_alphabet lore
+ * @c: the charcter in ASCII code
+ * Return: 1 is alphabet is lower else 0
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+return (1);
+else
+return (0);
+}
